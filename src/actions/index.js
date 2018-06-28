942be1301ebb661e6f83c16fe83ebb6f05dc6e94
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-28 12:49:27 +1000

    EDIT: selectBook action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
 export function selectBook(book) {
-  console.log('A book has been selected', book.title);
-  
+  // selectBook as an ActionCreator and needs to return an action,
+  // an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
